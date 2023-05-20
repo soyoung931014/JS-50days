4d117444bd6ded5bf4d578e9dd294ce58b49ab3c
park_so_young <soyoung931014@gmail.com>
2023-05-20 17:32:10 +0900

    hihi

---
+++ keyframe/main.js
@@ -0,0 +1,13 @@
+let mainText = document.querySelector("h1");
+
+// 스크롤의 높이에 따라 애니메이션이 들어왔다 나갔다를 해볼건데..
+// 그러면 스크롤의 높이를 일단 가져와야한다.
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  console.log(value);
+  if (value > 300) {
+    mainText.style.animation = "disappear 2s ease-out forwards"; // forwards : 영영사라지게..
+  } else {
+    mainText.style.animation = "slide 2s ease-out";
+  }
+});
